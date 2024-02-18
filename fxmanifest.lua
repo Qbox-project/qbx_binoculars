a78d4fb3e1450415a3a31aa0008ab2162c7ded25
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-02-18 11:56:53 -0500

    fix: remove unused module
    Removes the unused and deprecated utils module

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,7 @@ description 'qbx_binoculars'
 repository 'https://github.com/Qbox-project/qbx_binoculars'
 version '1.1.0'
 
-shared_scripts {
-	'@ox_lib/init.lua',
-	'@qbx_core/modules/utils.lua',
-}
+shared_script '@ox_lib/init.lua'
 
 client_scripts {
 	'@qbx_core/modules/playerdata.lua',
